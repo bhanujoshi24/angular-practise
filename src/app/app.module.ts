e3cebaad91161f3eafd27c4b5709aa38bb83576e
bhanujoshi24 <jbhanu244@gmail.com>
2022-04-19 22:21:17 +0530

    3 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -18,7 +20,9 @@ import { ProductListComponent } from './product-list/product-list.component';
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductAlertComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [
     AppComponent
